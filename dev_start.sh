a514f4d95be352d3c813007f4c1865c77f8cd763
Angel Barrientos <uetiko@gmail.com>
2017-09-15 18:15:53 -0500

    enviroment scriptwq

---
+++ dev_start.sh
@@ -0,0 +1,14 @@
+#!/bin/zsh
+#set -x
+
+echo -e "\e[1;32mActivando las variables de entorno  \e[0m"
+
+export URL_HOST='localhost'
+export DB_DATABASE='nulldatadb'
+export DB_NAME_TEST='test_nulldatadb'
+export DB_USERNAME='nulldata'
+export DB_HOST='localhost'
+export DB_PASSWORD='jSKzh6&&*Sc^3yv!44MM*3BeSKZ95c'
+export EMAIL_HOST_USER=''
+export EMAIL_HOST_PASSWD=''
+
