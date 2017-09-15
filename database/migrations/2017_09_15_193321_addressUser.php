ea8e65f7a847295dfb804d247bdcd9669a70fc50
Angel Barrientos <uetiko@gmail.com>
2017-09-15 15:04:07 -0500

    Add new migration for user's address

---
+++ database/migrations/2017_09_15_193321_addressUser.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddressUser extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('address', function(Blueprint $table){
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->string('calle', 40);
+            $table->string('estado', 30);
+            $table->string('delegacion_municipio', 30);
+            $table->integer('numero_ext');
+            $table->integer('numero_int');
+            $table->string('cp', 5);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('address');
+    }
+}
